713cbfe35522de36d28a5a47927cef36985c366e
Arpan Savaliya <arpansavaliya0609@gmail.com>
2026-02-25 15:44:59 -0800

    Trigger CI/CD pipeline

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from AWS CI/CD 🚀");
+  res.send("Deployed automatically via GitHub Actions ✅");
 });
 
 app.get("/health", (req, res) => {
